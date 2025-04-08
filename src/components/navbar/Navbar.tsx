3a0c6412f4072cb5e6376e882f88b602aa31e89a
JamilAmasa <jamilamasa@gmail.com>
2025-04-08 11:27:47 +0100

    feat(ui): setup shadcn

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from '../ui/button'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
+    // <div>Navbar</div>
+    <Button>This is a button</Button>
   )
 }
 
