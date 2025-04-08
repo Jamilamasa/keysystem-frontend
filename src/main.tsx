7968925542aaf2f9f36cadf825847ac912fb82e0
JamilAmasa <jamilamasa@gmail.com>
2025-04-08 11:31:34 +0100

    fix: setup absolute path

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { router } from './router';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div className='text-slate-400'>Hello world!</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
